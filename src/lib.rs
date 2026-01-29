df97125a145dffc6ad2aaa1dadf47004c3ef9d79
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2026-01-29 13:35:48 +0400

    added extensive exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,6 @@
 pub mod order_book;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use order_book::orderbook::{OrderBook, HalfBook};
+pub use order_book::matching_engine::MatchingEngine;
+pub use order_book::types::NewOrder;
+pub use order_book::tracing::Tracing;

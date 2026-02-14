73226a63f0af906864b146cc850d589406217ce1
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2026-02-14 11:00:20 +0400

    added qty upgrade for buy/sell

--- src/order_book/orderbook.rs
+++ src/order_book/orderbook.rs
@@ -35,6 +35,8 @@ impl OrderBook {
             if let Some(free_index) = self.bid.free_list.pop(){
                 self.bid.order_pool.insert(free_index, Some(order));
                 price_level.tail = free_index;
+                price_level.total_quantity += order_quantity;
+                price_level.order_count += 1;
                 if let Some(prev_order) = self.bid.order_pool.get_mut(price_level.tail).unwrap(){
                     prev_order.next = Some(free_index);
                 };
@@ -44,6 +46,8 @@ impl OrderBook {
             self.bid.order_pool.push(Some(order));
             let new_tail = self.bid.order_pool.len() - 1;
             price_level.tail = new_tail;
+            price_level.total_quantity += order_quantity;
+            price_level.order_count += 1;
             if let Some(prev_order) = self.bid.order_pool.get_mut(price_level.tail).unwrap(){
                 prev_order.next = Some(new_tail);
             };
